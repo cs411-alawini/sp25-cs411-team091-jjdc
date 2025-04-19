d374ac365ee846d6e4e5466116e5b1f19b930f6b
JSavree <jrliao2@illinois.edu>
2025-04-19 17:36:14 -0500

    "updated the base url call"

--- jjdc_recipe_tracker_proj/client/app/services/services.ts
+++ jjdc_recipe_tracker_proj/client/app/services/services.ts
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_BASE_URL || 'http://localhost:3000';
+const BASE_URL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:3007'; // process.env.REACT_APP_API_BASE_URL 
 
 
 export const httpClient = axios.create({
