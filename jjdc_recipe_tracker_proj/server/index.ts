2ce53f75c2b94918794cfaa2d078a8ec5d21c670
JSavree <jrliao2@illinois.edu>
2025-04-26 22:37:49 -0500

    "finished cookies and registering and login features"

--- jjdc_recipe_tracker_proj/server/index.ts
+++ jjdc_recipe_tracker_proj/server/index.ts
@@ -1,4 +1,6 @@
 import express, { Request, Response } from 'express';
+import session from 'express-session';
+import cookieParser from 'cookie-parser';
 import balanceBiteRoutes from './src/routes/balancebitesRoute';
 import cors from "cors";
 
@@ -7,6 +9,17 @@ const PORT = process.env.PORT || 3007;
 
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser());
+app.use(
+    session({
+      secret: "subscribe",
+      saveUninitialized: false,
+      resave: false,
+      cookie: {
+        maxAge: 60 * 60 * 24 * 30,
+      },
+    })
+  );
 
 app.get('/api', (req: Request, res: Response) => {
     res.send('API of BalanceBite');
@@ -16,4 +29,9 @@ app.use('/api/balancebites', balanceBiteRoutes);
 
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
+});
+
+app.get('/', (req: Request, res: Response) => {
+    console.log(req.session);
+    console.log(req.session.id);
 });
